4f33b33131f3636dcabc367169dba3876f056ca1
annetpf <annetausmag@gmail.com>
2016-03-05 12:34:33 +0500

    programm generation names of numbers

--- index.php
+++ index.php
@@ -26,40 +26,124 @@ $table .= '
 		</tr>
 	</thead>';
 
-// Array of random numbers.
-$randomArray = range(1, $rows);
-// Shaffle array to eliminate dublicates.
-shuffle($randomArray); 
-
-// Run through array...
-foreach ($randomArray as $val) {
+for ($tr=1; $tr<=$rows; $tr++) {
 	
-	// This variable is used to detect number name.
-	$numClass = $lang[$val];
+	$tdClass = "smth";
+	
+	// Create random number.
+	$rand = rand();
+	// Covrert to string.
+	$str = "$rand";
+	// Convert to array.
+	$arr1 = str_split($str);
+	// Check length of array to define length of number.
+	$length = count($arr1);
+
+	// And now check number...
+
+	// 0 - 9
+	if ($length == 1) {
+		$numClass = $units[$rand];
+	}
+
+	// 10 - 19
+	if ($length == 2 && $arr1[0] == '1') {
+		$numClass = $oneDozensOf[$rand];
+	}
+
+	// 20 - 99
+	if ($length == 2 && $arr1[0] != '1') {
+		$numClass = $dozensOf[$arr1[0]] .' ' .$units[$arr1[1]];
+		if ($length == 2 && $arr1[1] == '0') {
+			$numClass = $dozensOf[$arr1[0]];
+		}
+	}
+
+	// 100 - 999
+	if ($length == 3) {
+		$numClass = $units[$arr1[0]] .' hundred and ' .$dozensOf[$arr1[1]] .' ' .$units[$arr1[2]];
+		// If 1 - the second number (211, 213, 214...)
+		if ($arr1[1] == '1') {
+			$numClass = $units[$arr1[0]] .' hundred and ' .$oneDozensOf[$arr1[1] .$arr1[2]];			
+		}
+	}
+
+	// 1000 - 9999
+	if ($length == 4) {
+		$numClass = $units[$arr1[0]] .' thousand ' .$units[$arr1[1]] .' hundred and ' .$dozensOf[$arr1[2]] . ' ' .$units[$arr1[3]];
+		// Numbers like 2026, 3031... (when 0 is the second symbol)
+		if ($arr1[1] == '0' && $arr1[2] != '1') {
+			$numClass = $units[$arr1[0]] .' thousand '  .$dozensOf[$arr1[2]] . ' ' .$units[$arr1[3]];
+		}
+		// Numbers 2012, 4013, 4018... (when 1 is the third symbol)
+		else if ($arr1[2] == '1') {
+			$numClass = $units[$arr1[0]] .' thousand '  .$oneDozensOf[$arr1[2].$arr1[3]];
+		}
+	}
+
+	// 10000 - 99999
+	if ($length == 5) {
+		// Normal numbers
+		$numClass = $dozensOf[$arr1[0]] .' ' .$units[$arr1[1]]. ' thousand ' .$units[$arr1[2]] .' hundred and ' .$dozensOf[$arr1[3]] . ' ' .$units[$arr1[4]];
+		// No hundred part.
+		if ($arr1[2] == '0') {
+			$numClass =  $dozensOf[$arr1[0]] .' ' .$units[$arr1[1]] .' thousand and ' .$dozensOf[$arr1[3]] . ' ' .$units[$arr1[4]];
+			// 1 - fourth number.
+			if ($arr1[3] == '1') {
+				$numClass = $dozensOf[$arr1[0]] .' ' .$units[$arr1[1]] .' thousand and ' .$oneDozensOf[$arr1[3] .$arr1[4]];
+			}
+		}
+
+		// 1 - first number.
+		if ($arr1[0] == '1') {
+			$numClass = $oneDozensOf[$arr1[0] .$arr1[1]] .' thousand ' .$units[$arr1[2]] .' hundred and ' .$dozensOf[$arr1[3]] . ' ' .$units[$arr1[4]];
+			// No hundred part.
+			if ($arr1[2] == '0') {
+				$numClass = $oneDozensOf[$arr1[0] .$arr1[1]] .' thousand and ' .$dozensOf[$arr1[3]] . ' ' .$units[$arr1[4]];
+				// 1 - fourth number.
+				if ($arr1[3] == '1') {
+					$numClass = $oneDozensOf[$arr1[0] .$arr1[1]] .' thousand and ' .$oneDozensOf[$arr1[3] .$arr1[4]];
+				}
+			}
+		}
+
+		// 0 - second number.
+		if ($arr1[1] == '0') {
+			$numClass = $dozensOf[$arr1[0]] .' thousand ' .$units[$arr1[2]] .' hundred and ' .$dozensOf[$arr1[3]] . ' ' .$units[$arr1[4]];
+			// No hundred part.
+			if ($arr1[2] == '0') {
+				$numClass = $dozensOf[$arr1[0]] .' thousand and ' .$dozensOf[$arr1[3]] . ' ' .$units[$arr1[4]];
+				// 1 - fourth number.
+				if ($arr1[3] == '1') {
+					$numClass = $dozensOf[$arr1[0]] .' thousand and ' .$oneDozensOf[$arr1[3] .$arr1[4]];
+				}
+			}
+		}
+	}
+
 
 	// Check is number divisible by 3 and 5
-	if ($val %15 == 0)  {
+	if ($rand %15 == 0)  {
 		$tdClass = "divisible_3_5";
 		$numClass = "Fizzbuzz";
 	} 
 	// Check is number divisible by 5
-	else if ($val %5 == 0) {
+	else if ($rand %5 == 0) {
 		$tdClass = "divisible_5";
 		$numClass = "buzz";
 	} 
 	// Check is number divisible by 3
-	else if ($val %3 == 0) {
+	else if ($rand %3 == 0) {
 		$tdClass = "divisible_3";
 		$numClass = "Fizz";
 	}
 
 	// Render rows. First - with numbers...
-	$table .= '<td class=' .$tdClass .'>' .$val .'</td>';
+	$table .= '<td class=' .$tdClass .'>' .$rand .'</td>';
 	// ...second - width definition.
 	$table .= '<td>' .$numClass .'</td>';
-	$table .= '</tr>';
-
- }
+	$table .= '</tr>';	
+}
 
 // Close table.
 $table .= '</table>';
--- languages/lang.en.php
+++ languages/lang.en.php
@@ -2,333 +2,46 @@
 
 <?php
 
+$units = array();
+
+$units['0'] = 'zero';
+$units['1'] = 'one';
+$units['2'] = 'two';
+$units['3'] = 'three';
+$units['4'] = 'four';
+$units['5'] = 'five';
+$units['6'] = 'six';
+$units['7'] = 'seven';
+$units['8'] = 'eight';
+$units['9'] = 'nine';
+
+$oneDozensOf = array();
+
+$oneDozensOf['10'] = 'ten';
+$oneDozensOf['11'] = 'eleven';
+$oneDozensOf['12'] = 'tweleve';
+$oneDozensOf['13'] = 'thirteen';
+$oneDozensOf['14'] = 'fourteen';
+$oneDozensOf['15'] = 'fifteen';
+$oneDozensOf['16'] = 'sixteen';
+$oneDozensOf['17'] = 'seventeen';
+$oneDozensOf['18'] = 'eighteen';
+$oneDozensOf['19'] = 'nineteen';
+
+$dozensOf = array();
+
+$dozensOf['2'] = 'twenty';
+$dozensOf['3'] = 'thirty';
+$dozensOf['4'] = 'forty';
+$dozensOf['5'] = 'fifty';
+$dozensOf['6'] = 'sixty';
+$dozensOf['7'] = 'seventy';
+$dozensOf['8'] = 'eighty';
+$dozensOf['9'] = 'ninety';
+
 $lang = array();
 
-$lang['firstColumnTitle'] = 'Numbers';
-$lang['secondColumnTitle'] = 'Feature';
-
-$lang['1'] = 'One';
-$lang['2'] = 'Two';
-$lang['4'] = 'Four';
-$lang['7'] = 'Seven';
-$lang['8'] = 'Eight';
-$lang['11'] = 'Eleven';
-$lang['13'] = 'Thirteen';
-$lang['14'] = 'Fourteen';
-$lang['16'] = 'Sixteen';
-$lang['17'] = 'Seventeen';
-$lang['19'] = 'Nineteen';
-
-$lang['22'] = 'Twenty two';
-$lang['23'] = 'Twenty three';
-$lang['26'] = 'Twenty six';
-$lang['28'] = 'Twenty eight';
-$lang['29'] = 'Twenty nine';
-
-
-$lang['31'] = 'Thirty one';
-$lang['32'] = 'Thirty two';
-$lang['34'] = 'Thirty four';
-$lang['37'] = 'Thirty seven';
-$lang['38'] = 'Thirty eight';
-
-$lang['41'] = 'Thirty one';
-$lang['42'] = 'Thirty two';
-$lang['43'] = 'Thirty three';
-$lang['44'] = 'Thirty four';
-$lang['46'] = 'Thirty six';
-$lang['47'] = 'Thirty seven';
-$lang['49'] = 'Thirty nine';
-
-$lang['51'] = 'Fifty one';
-$lang['52'] = 'Fifty two';
-$lang['53'] = 'Fifty three';
-$lang['56'] = 'Fifty six';
-$lang['58'] = 'Fifty eight';
-$lang['59'] = 'Fifty nine';
-
-$lang['61'] = 'Sixty one';
-$lang['62'] = 'Sixty two';
-$lang['64'] = 'Sixty four';
-$lang['67'] = 'Sixty seven';
-$lang['68'] = 'Sixty eight';
-
-$lang['71'] = 'Seventy one';
-$lang['73'] = 'Seventy three';
-$lang['74'] = 'Seventy four';
-$lang['76'] = 'Seventy six';
-$lang['77'] = 'Seventy seven';
-$lang['79'] = 'Seventy nine';
-
-$lang['81'] = 'Eighty one';
-$lang['82'] = 'Eighty two';
-$lang['83'] = 'Eighty three';
-$lang['86'] = 'Eighty six';
-$lang['87'] = 'Eighty seven';
-$lang['88'] = 'Eighty eight';
-$lang['89'] = 'Eighty nine';
-
-$lang['91'] = 'Ninety one';
-$lang['92'] = 'Ninety two';
-$lang['94'] = 'Ninety four';
-$lang['97'] = 'Ninety seven';
-$lang['98'] = 'Ninety eight';
-
-$lang['101'] = 'One hundred and one';
-$lang['103'] = 'One hundred and three';
-$lang['104'] = 'One hundred and four';
-$lang['106'] = 'One hundred and six';
-$lang['107'] = 'One hundred and seven';
-$lang['109'] = 'One hundred and nine';
-
-$lang['111'] = 'One hundred eleven';
-$lang['112'] = 'One hundred twelve';
-$lang['113'] = 'One hundred thirteen';
-$lang['116'] = 'One hundred sixteen';
-$lang['118'] = 'One hundred eighteen';
-$lang['119'] = 'One hundred nineteen';
-
-$lang['121'] = 'One hundred and twenty one';
-$lang['122'] = 'One hundred and twenty two';
-$lang['124'] = 'One hundred and twenty four';
-$lang['127'] = 'One hundred and twenty seven';
-$lang['128'] = 'One hundred and twenty eight';
-
-$lang['131'] = 'One hundred and thirty one';
-$lang['133'] = 'One hundred and thirty three';
-$lang['134'] = 'One hundred and thirty four';
-$lang['136'] = 'One hundred and thirty six';
-$lang['137'] = 'One hundred and thirty seven';
-$lang['139'] = 'One hundred and thirty nine';
-
-$lang['142'] = 'One hundred and forty two';
-$lang['143'] = 'One hundred and forty three';
-$lang['146'] = 'One hundred and forty six';
-$lang['148'] = 'One hundred and forty eight';
-$lang['149'] = 'One hundred and forty nine';
-
-$lang['151'] = 'One hundred and fifty one';
-$lang['152'] = 'One hundred and fifty two';
-$lang['154'] = 'One hundred and fifty four';
-$lang['157'] = 'One hundred and fifty seven';
-$lang['158'] = 'One hundred and fifty eight';
-
-$lang['161'] = 'One hundred and sixty one';
-$lang['163'] = 'One hundred and sixty three';
-$lang['164'] = 'One hundred and sixty four';
-$lang['166'] = 'One hundred and sixty six';
-$lang['167'] = 'One hundred and sixty seven';
-$lang['169'] = 'One hundred and sixty nine';
-
-$lang['171'] = 'One hundred and seventy one';
-$lang['172'] = 'One hundred and seventy two';
-$lang['173'] = 'One hundred and seventy three';
-$lang['176'] = 'One hundred and seventy six';
-$lang['178'] = 'One hundred and seventy eight';
-$lang['179'] = 'One hundred and seventy nine';
-
-$lang['181'] = 'One hundred and eighty one';
-$lang['182'] = 'One hundred and eighty two';
-$lang['184'] = 'One hundred and eighty four';
-$lang['187'] = 'One hundred and eighty seven';
-$lang['188'] = 'One hundred and eighty eight';
-
-$lang['191'] = 'One hundred and ninety one';
-$lang['193'] = 'One hundred and ninety three';
-$lang['194'] = 'One hundred and ninety four';
-$lang['196'] = 'One hundred and ninety six';
-$lang['197'] = 'One hundred and ninety seven';
-$lang['199'] = 'One hundred and ninety nine';
-
-$lang['202'] = 'Two hundred and two';
-$lang['203'] = 'Two hundred and three';
-$lang['206'] = 'Two hundred and six';
-$lang['208'] = 'Two hundred and eight';
-$lang['209'] = 'Two hundred and nine';
-
-$lang['211'] = 'Two hundred and eleven';
-$lang['212'] = 'Two hundred and twelve';
-$lang['214'] = 'Two hundred and fourteen';
-$lang['217'] = 'Two hundred and seventeen';
-$lang['218'] = 'Two hundred and eighteen';
-
-$lang['221'] = 'Two hundred and twenty one';
-$lang['223'] = 'Two hundred and twenty three';
-$lang['224'] = 'Two hundred and twenty four';
-$lang['226'] = 'Two hundred and twenty six';
-$lang['227'] = 'Two hundred and twenty seven';
-$lang['229'] = 'Two hundred and twenty nine';
-
-$lang['232'] = 'Two hundred and thirty two';
-$lang['233'] = 'Two hundred and thirty three';
-$lang['236'] = 'Two hundred and thirty six';
-$lang['238'] = 'Two hundred and thirty eight';
-$lang['239'] = 'Two hundred and thirty nine';
-
-$lang['241'] = 'Two hundred and forty one';
-$lang['242'] = 'Two hundred and forty two';
-$lang['244'] = 'Two hundred and forty four';
-$lang['247'] = 'Two hundred and forty seven';
-$lang['248'] = 'Two hundred and forty eight';
-
-$lang['251'] = 'Two hundred and fifty one';
-$lang['253'] = 'Two hundred and fifty three';
-$lang['254'] = 'Two hundred and fifty four';
-$lang['256'] = 'Two hundred and fifty шнсть';
-$lang['257'] = 'Two hundred and fifty seven';
-$lang['259'] = 'Two hundred and fifty nine';
-
-$lang['262'] = 'Two hundred and sixty two';
-$lang['263'] = 'Two hundred and sixty three';
-$lang['266'] = 'Two hundred and sixty six';
-$lang['268'] = 'Two hundred and sixty eight';
-$lang['269'] = 'Two hundred and sixty nine';
-
-$lang['271'] = 'Two hundred and seventy one';
-$lang['272'] = 'Two hundred and seventy two';
-$lang['274'] = 'Two hundred and seventy four';
-$lang['277'] = 'Two hundred and seventy seven';
-$lang['278'] = 'Two hundred and seventy eight';
-$lang['279'] = 'Two hundred and seventy nine';
-
-$lang['281'] = 'Two hundred and eighty one';
-$lang['283'] = 'Two hundred and eighty three';
-$lang['284'] = 'Two hundred and eighty four';
-$lang['286'] = 'Two hundred and eighty six';
-$lang['287'] = 'Two hundred and eighty seven';
-$lang['289'] = 'Two hundred and eighty nine';
-
-$lang['292'] = 'Two hundred and ninety two';
-$lang['293'] = 'Two hundred and ninety three';
-$lang['296'] = 'Two hundred and ninety six';
-$lang['298'] = 'Two hundred and ninety eight';
-$lang['299'] = 'Two hundred and ninety nine';
-
-$lang['301'] = 'Three hundred and one';
-$lang['302'] = 'Three hundred and two';
-$lang['304'] = 'Three hundred and four';
-$lang['307'] = 'Three hundred and seven';
-$lang['308'] = 'Three hundred and eight';
-
-$lang['311'] = 'Three hundred and eleven';
-$lang['313'] = 'Three hundred and thirteen';
-$lang['314'] = 'Three hundred and fourteen';
-$lang['316'] = 'Three hundred and sixteen';
-$lang['317'] = 'Three hundred and seventeen';
-$lang['319'] = 'Three hundred and nineteen';
-
-$lang['322'] = 'Three hundred and twenty two';
-$lang['323'] = 'Three hundred and twenty three';
-$lang['326'] = 'Three hundred and twenty six';
-$lang['328'] = 'Three hundred and twenty eight';
-$lang['329'] = 'Three hundred and twenty nine';
-
-$lang['331'] = 'Three hundred and thirty one';
-$lang['332'] = 'Three hundred and thirty two';
-$lang['334'] = 'Three hundred and thirty four';
-$lang['337'] = 'Three hundred and thirty seven';
-$lang['338'] = 'Three hundred and thirty eight';
-
-$lang['341'] = 'Three hundred and forty one';
-$lang['343'] = 'Three hundred and forty three';
-$lang['344'] = 'Three hundred and forty four';
-$lang['346'] = 'Three hundred and forty six';
-$lang['347'] = 'Three hundred and forty seven';
-$lang['349'] = 'Three hundred and forty nine';
-
-$lang['352'] = 'Three hundred and fifty two';
-$lang['353'] = 'Three hundred and fifty three';
-$lang['356'] = 'Three hundred and fifty six';
-$lang['358'] = 'Three hundred and fifty eight';
-$lang['359'] = 'Three hundred and fifty nine';
-
-$lang['361'] = 'Three hundred and sixty one';
-$lang['362'] = 'Three hundred and sixty two';
-$lang['364'] = 'Three hundred and sixty four';
-$lang['367'] = 'Three hundred and sixty seven';
-$lang['368'] = 'Three hundred and sixty eight';
-
-$lang['371'] = 'Three hundred and seventy one';
-$lang['373'] = 'Three hundred and seventy three';
-$lang['374'] = 'Three hundred and seventy four';
-$lang['376'] = 'Three hundred and seventy six';
-$lang['377'] = 'Three hundred and seventy seven';
-$lang['379'] = 'Three hundred and seventy nine';
-
-$lang['382'] = 'Three hundred and eighty two';
-$lang['383'] = 'Three hundred and eighty three';
-$lang['386'] = 'Three hundred and eighty six';
-$lang['388'] = 'Three hundred and eighty eight';
-$lang['389'] = 'Three hundred and eighty nine';
-
-$lang['391'] = 'Three hundred and ninety one';
-$lang['392'] = 'Three hundred and ninety two';
-$lang['394'] = 'Three hundred and ninety four';
-$lang['397'] = 'Three hundred and ninety seven';
-$lang['398'] = 'Three hundred and ninety eight';
-
-$lang['401'] = 'Four hundred and one';
-$lang['403'] = 'Four hundred and three';
-$lang['404'] = 'Four hundred and four';
-$lang['406'] = 'Four hundred and six';
-$lang['407'] = 'Four hundred and seven';
-$lang['409'] = 'Four hundred and nine';
-
-$lang['412'] = 'Four hundred and twelve';
-$lang['413'] = 'Four hundred and thirteen';
-$lang['416'] = 'Four hundred and sixteen';
-$lang['418'] = 'Four hundred and eighteen';
-$lang['419'] = 'Four hundred and nineteen';
-
-$lang['421'] = 'Four hundred and twenty one';
-$lang['422'] = 'Four hundred and twenty two';
-$lang['424'] = 'Four hundred and twenty four';
-$lang['427'] = 'Four hundred and twenty seven';
-$lang['428'] = 'Four hundred and twenty eight';
-
-$lang['431'] = 'Four hundred and thirty one';
-$lang['433'] = 'Four hundred and thirty three';
-$lang['434'] = 'Four hundred and thirty four';
-$lang['436'] = 'Four hundred and thirty six';
-$lang['437'] = 'Four hundred and thirty seven';
-$lang['439'] = 'Four hundred and thirty nine';
-
-$lang['442'] = 'Four hundred and forty two';
-$lang['443'] = 'Four hundred and forty three';
-$lang['446'] = 'Four hundred and forty six';
-$lang['448'] = 'Four hundred and forty eight';
-$lang['449'] = 'Four hundred and forty nine';
-
-$lang['451'] = 'Four hundred and fifty one';
-$lang['452'] = 'Four hundred and fifty two';
-$lang['454'] = 'Four hundred and fifty four';
-$lang['457'] = 'Four hundred and fifty seven';
-$lang['458'] = 'Four hundred and fifty eight';
-
-$lang['461'] = 'Four hundred and sixty one';
-$lang['463'] = 'Four hundred and sixty three';
-$lang['464'] = 'Four hundred and sixty four';
-$lang['466'] = 'Four hundred and sixty six';
-$lang['467'] = 'Four hundred and sixty seven';
-$lang['469'] = 'Four hundred and sixty nine';
-
-$lang['472'] = 'Four hundred and seventy two';
-$lang['473'] = 'Four hundred and seventy three';
-$lang['476'] = 'Four hundred and seventy six';
-$lang['478'] = 'Four hundred and seventy eight';
-$lang['479'] = 'Four hundred and seventy nine';
-
-$lang['481'] = 'Four hundred and eighty one';
-$lang['482'] = 'Four hundred and eighty two';
-$lang['484'] = 'Four hundred and eighty four';
-$lang['487'] = 'Four hundred and eighty seven';
-$lang['488'] = 'Four hundred and eighty eight';
-
-$lang['491'] = 'Four hundred and ninety one';
-$lang['493'] = 'Four hundred and ninety three';
-$lang['494'] = 'Four hundred and ninety four';
-$lang['496'] = 'Four hundred and ninety six';
-$lang['497'] = 'Four hundred and ninety seven';
-$lang['499'] = 'Four hundred and ninety nine';
+$lang['firstColumnTitle'] = 'Число';
+$lang['secondColumnTitle'] = 'Характеристика';
 
 ?>
